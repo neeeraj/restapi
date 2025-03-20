76bee0c3876c1a58d75648fc3149129962ca883a
neeeraj <neerajpujari86@gmail.com>
2025-03-20 18:01:16 +0530

    Add files via upload

--- Api.py
+++ Api.py
@@ -1,6 +1,11 @@
+import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Flask API is running!"
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -8,4 +13,5 @@ def predict():
     return jsonify({"message": f"Received: {data}"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # ✅ Render ke PORT variable ko use karo
+    app.run(host='0.0.0.0', port=port)
